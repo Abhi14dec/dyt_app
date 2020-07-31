fd840e397647e6c0b8b820c9000b511e9deea169
Abhinav Kumar <kumarabhinav167@gmail.com>
2020-08-01 02:52:10 +0530

    dyt app updated

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -6,10 +6,10 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <a><i className="fa fa-bars" aria-hidden="true"></i></a>
-            <a href="#home"><i className="fa fa-fw fa-home"></i></a>
-            <a href="#services"><i className="fa fa-fw fa-wrench"></i></a>
-            <a href="#clients"><i className="fa fa-fw fa-user"></i></a>
-            <a href="#contact"><i className="fa fa-fw fa-envelope"></i></a>
+            <a href="#home"><i class="fa fa-volume-up" aria-hidden="true"></i></a>
+            <a href="#database"><i className="fa fa-database" aria-hidden="true"></i></a>
+            <a href="#clients"><i className="fa fa-fw fa-user" aria-hidden="true"></i></a>
+            <a href="#settings"><i className="fa fa-cog" aria-hidden="true"></i></a>
           
         </div>
     );
